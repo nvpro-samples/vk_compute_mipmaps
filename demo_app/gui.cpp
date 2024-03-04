d78f452d7284fc5487e55f78957b02595fa65602
Martin-Karl Lefrançois <mlefrancois@nvidia.com>
2024-03-04 14:48:28 +0100

    Bulk Change [2024-03-04]
    Script sync from SHA: ef8bc3df1505b59c715ad7fb7b2dab035b8e28fa

--- demo_app/gui.cpp
+++ demo_app/gui.cpp
@@ -54,23 +54,23 @@ void Gui::cmdInit(VkCommandBuffer cmdBuf, GLFWwindow* pWindow, const nvvk::Conte
   assert(m_pool == VK_NULL_HANDLE);
   NVVK_CHECK(vkCreateDescriptorPool(ctx, &poolInfo, nullptr, &m_pool));
 
-  ImGui_ImplVulkan_InitInfo info{ctx.m_instance,
-                                 ctx.m_physicalDevice,
-                                 ctx.m_device,
-                                 frameManager.getQueueFamilyIndex(),
-                                 frameManager.getQueue(),
-                                 VK_NULL_HANDLE,  // No need for pipeline cache for simple sample.
-                                 m_pool,
-                                 subpass,
-                                 frameManager.getSwapChain().getImageCount(),
-                                 frameManager.getSwapChain().getImageCount(),
-                                 VK_SAMPLE_COUNT_1_BIT,
-                                 false,
-                                 VK_FORMAT_UNDEFINED,
-                                 nullptr,
-                                 [](VkResult err) { NVVK_CHECK(err); }};
+  ImGui_ImplVulkan_InitInfo info{};
+  info.Instance            = ctx.m_instance;
+  info.PhysicalDevice      = ctx.m_physicalDevice;
+  info.Device              = ctx.m_device;
+  info.QueueFamily         = frameManager.getQueueFamilyIndex();
+  info.Queue               = frameManager.getQueue();
+  info.DescriptorPool      = m_pool;
+  info.RenderPass          = renderPass;
+  info.Subpass             = subpass;
+  info.MinImageCount       = frameManager.getSwapChain().getImageCount();
+  info.ImageCount          = frameManager.getSwapChain().getImageCount();
+  info.MSAASamples         = VK_SAMPLE_COUNT_1_BIT;
+  info.UseDynamicRendering = false;
+  info.Allocator           = nullptr;
+  info.CheckVkResultFn     = [](VkResult err) { NVVK_CHECK(err); };
 
-  ImGui_ImplVulkan_Init(&info, renderPass);
+  ImGui_ImplVulkan_Init(&info);
   ImGui_ImplVulkan_CreateFontsTexture();
 
   ImGui_ImplGlfw_InitForVulkan(pWindow, false);
@@ -383,7 +383,7 @@ void Gui::updateFpsSample()
 void Gui::zoomCallback2d(double dy)
 {
   glm::vec2 mouseTexCoord = m_cam.offset + m_cam.scale * glm::vec2(m_zoomMouseX, m_zoomMouseY);
-  float        scale         = expf(float(dy));
+  float     scale         = expf(float(dy));
   m_cam.scale *= scale;
 
   glm::vec2 wrongTexCoord = m_cam.offset + m_cam.scale * glm::vec2(m_zoomMouseX, m_zoomMouseY);
